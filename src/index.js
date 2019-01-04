7f56fe8d031ed9f8f5fb931700a7dd3020788f3c
Benjamin <benjaminragot53000@gmail.com>
2019-01-04 22:39:57 +0100

    add new web config

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
+import "./web.config";
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
