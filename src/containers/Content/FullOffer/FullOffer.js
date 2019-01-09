1e06ddd48352fa0839a292f671467ec8f2302ab6
Benjamin <benjaminragot53000@gmail.com>
2019-01-09 21:21:36 +0100

    made styling for fulloffer

--- src/containers/Content/FullOffer/FullOffer.js
+++ src/containers/Content/FullOffer/FullOffer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-
+import Box from '../../../components/Box/Box';
+import Title from '../../../components/Title/Title';
 
 import './FullOffer.css';
 
@@ -48,9 +49,58 @@ class FullPost extends Component {
         
         if ( this.state.loadedPost ) {
             post = (
-                <div className="FullOffer">
-                    <h1>{this.state.loadedPost.CAST_DESCRIPTION_POSTE}</h1>
-                </div>
+            <div className="container">
+                <Box color="#1F3040" textColor="white">
+                    <Title align="left" color="#AFE9FF" fade="#AFE9FF">
+                        {this.state.loadedPost.CAST_INTITULE} - {this.state.loadedPost.CAST_REFERENCE}
+                    </Title>
+                    <div>
+                        <b>Nom du demandeur :</b> {this.state.loadedPost.PRO_NAME}                    
+                    </div>
+                    <div>
+                        <b>Numéro de téléphone :</b> {this.state.loadedPost.CTC_NUM_TEL}                    
+                    </div>
+                    <div>
+                        <b>Numéro de fax :</b> {this.state.loadedPost.CTC_NUM_FAX}                    
+                    </div>
+                    <div>
+                        <b>Email :</b> {this.state.loadedPost.CTC_EMAIL}                    
+                    </div>
+                    <div>
+                        <b>Métier :</b> {this.state.loadedPost.MET_LIBELLE}                    
+                    </div>
+                    <div>
+                        <b>Domaine :</b> {this.state.loadedPost.DOM_LIBELLE}                    
+                    </div>
+                    <div>
+                        <b>Localisation :</b> {this.state.loadedPost.LOC_LIBELLE}                    
+                    </div>
+                    <div>
+                        <b>Contrat :</b> {this.state.loadedPost.CON_LIBELLE}                    
+                    </div>
+                    <div>
+                        <b>Description du poste :</b> {this.state.loadedPost.CAST_DESCRIPTION_POSTE}                    
+                    </div>
+                    <div>
+                        <b>Description du profil :</b> {this.state.loadedPost.CAST_DESCRIPTION_PROFIL}                    
+                    </div>
+                    <div>
+                        <b>Description du profil :</b> {this.state.loadedPost.CAST_DESCRIPTION_PROFIL}                    
+                    </div>
+                    <div>
+                        <b>Description du profil :</b> {this.state.loadedPost.CAST_DESCRIPTION_PROFIL}                    
+                    </div>
+                    <div>
+                        <b>Date de début publication :</b> {this.state.loadedPost.CAST_DATE_DEBUT_PUBLICATION}                    
+                    </div>
+                    <div>
+                        <b>Date de début de contrat :</b> {this.state.loadedPost.CAST_DATE_DEBUT_CONTRAT}                    
+                    </div>
+                    <div>
+                        <b>Durée :</b> {this.state.loadedPost.CAST_DUREE_DIFFUSION} jours                    
+                    </div>
+                </Box>
+            </div>
             );
         }
         return post;
